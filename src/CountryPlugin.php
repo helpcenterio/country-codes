0d20f36ce679dd8b8dcf1f06b64b4b06d07669c4
Ivan Penchev <ivanvpenchev@gmail.com>
2022-12-27 14:09:34 +0200

    Fix Composer 2 plugin support.

--- src/CountryPlugin.php
+++ src/CountryPlugin.php
@@ -166,4 +166,30 @@ class CountryPlugin implements PluginInterface, EventSubscriberInterface
     {
         // no action required
     }
+
+    /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @return void
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // no action required
+    }
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @return void
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // no action required
+    }
 }
